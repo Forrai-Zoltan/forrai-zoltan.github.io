95b7c13ead77097be3371acce25afbf04d48e6d6
simon-forman <forrai.zoltan66@gmail.com>
2025-06-30 02:20:18 +0200

    add rhyme to secret page

--- script.js
+++ script.js
@@ -21,13 +21,48 @@ window.addEventListener("resize", () => {
   main.style.minHeight = `${viewportHeight - headerHeight - 23}px`;
 });
 
-// Insert horizontal line paragraph into every article > section
-const ch65 = document.createElement("p");
-ch65.id = "ch65";
-ch65.textContent =
-  "-------------------------------------------------------------------------------------------------";
+//SECRET GAME PASSWORD
+const riddlePassword = document.getElementById("riddle-password");
+const feedback = document.getElementById("secret-feedback");
+const responses = [
+  "Nice try...",
+  "Incorrect...",
+  "Not quite...",
+  "Wrong password...",
+  "Clue: look harder!",
+  "The aswer is: gullible",
+  "Nope, sorry. Try again!",
+  "Incorrect... Try again!",
+  "Maybe a few more tries...",
+  "That one was almost right...",
+  "I think you misspelled that one...",
+  "You can't brute force this one mate...",
+  "The answer is closer than you think...",
+  "That was correct! Wait, no it wasn't...",
+  "The dolphin of the east only wakes once...",
+];
 
-const lastSection = document.querySelector("article > section:last-of-type");
-if (lastSection) {
-  lastSection.appendChild(ch65);
-}
+document.getElementById("secret-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const input = riddlePassword.value.trim().toLowerCase();
+  let randomResponse = responses[Math.floor(Math.random() * responses.length)];
+
+  if (input === "gullible") {
+    randomResponse = "You really tried that?";
+    feedback.style.visibility = "visible";
+  }
+  if (input === "sound") {
+    randomResponse = "Clever! But no, that ain't it.";
+    feedback.style.visibility = "visible";
+  }
+
+  feedback.textContent = randomResponse;
+  feedback.style.visibility = "visible";
+});
+
+riddlePassword.addEventListener("input", function () {
+  if (riddlePassword.value.trim() === "") {
+    feedback.style.visibility = "hidden";
+  }
+});
