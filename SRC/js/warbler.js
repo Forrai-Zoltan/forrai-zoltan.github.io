aa79373a1cabe97207c85f623b88d5484b856709
Gildrom <forrai.zoltan66@gmail.com>
2026-06-25 16:35:57 +0200

    major reorganisations

--- SRC/js/warbler.js
+++ SRC/js/warbler.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const html = `
       <interaction-row>
         <input type="checkbox" class="heart" id="${uniqueId}" />
-        <label for="${uniqueId}">
+        <label tabindex="0" for="${uniqueId}">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
           </svg>
@@ -39,6 +39,14 @@ document.addEventListener("DOMContentLoaded", () => {
         delete heartStates[uniqueId];
       }
       localStorage.setItem("heartStates", JSON.stringify(heartStates));
+    });
+    const label = document.querySelector(`label[for="${uniqueId}"]`);
+    label.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        checkbox.checked = !checkbox.checked;
+        checkbox.dispatchEvent(new Event("change"));
+      }
     });
   });
 });
